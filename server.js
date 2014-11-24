6afccab089878dd8b18221de9783ea054fecfe67
Rajesh Veeranki <rveeranki01@gmail.com>
2014-11-24 23:06:24 +0530

    Created a node thrift client

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var log4js = require('log4js');
 var thrift = require('thrift');
+var StoreService = require("./gen-nodejs/StoreService"),
+        user_types = require("./gen-nodejs/user_types");
+
 
 var JAVA_SERVER_ADDRESS = "127.0.0.1";
 var JAVA_SERVER_PORT = "9090";
 var LISTENER_PORT = "8080";
+
+//Creating thrift client
+var connection = thrift.createConnection(JAVA_SERVER_ADDRESS,JAVA_SERVER_PORT, {
+	  transport : thrift.TFramedTransport,
+      	  protocol : thrift.TBinaryProtocol
+});
+
+connection.on('error', function(err) {
+	  err.stack;
+	  //assert(false, err);
+});
+
+var client = thrift.createClient(StoreService, connection);
+
+//
 
 var app = express();
 
@@ -16,11 +34,18 @@ app.post('/receive', function(req,res) {
 console.log("... Starting server at port "+LISTENER_PORT+"...")
 app.listen(LISTENER_PORT);
 
-var send(req) {
-	console.log("Parsing data");
+var send = function (req) {
+	console.log("Parsing data "+req);
+	var json = {};
 	try {
-	  var json = JSON.stringify(req);
+	  json = JSON.stringify(req);
 	}catch(err) {
-		
+		err.stack;
 	 }
+	var user = new user_types.User(json);
 }
+
+var repeat = setInterval(function() {
+	var string = "{ name: 'veer' , email: 'user@email.com' , comment: 'This is a test' , id: 1}"
+	send(string);
+},1000);
